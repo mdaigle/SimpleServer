0edf1180726c33747abfafb744695e20d8621898
Malcolm Daigle <daiglemalcolm@gmail.com>
2016-04-05 21:36:10 -0700

    Implements coordination of server shutdown.

--- server/server.go
+++ server/server.go
@@ -7,27 +7,74 @@ import(
 	"os"
 	"time"
 	"sync"
+	"bufio"
+	"io"
 )
+
+//TODO: error checking
 
 func main() {
 	portnum := os.Args[1]
 	udpAddr, err := net.ResolveUDPAddr("up4", portnum)
 	var wg sync.WaitGroup
+	quit := make(chan bool)
+	end := make(chan bool)
+
+	go broadcastQuit(quit, end)
+	go readIn(end)
 
 	for {
-		// do select to listen for quit command "q"
-		// when we hear it, post to a shared channel that we're quitting
 		conn, err := net.ListenUDP("udp", udpAddr)
-		wg.Add(1)
-		go func() {
-			handleClient(conn)
-			wg.Done()
-		}()
+		if err == nil {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				handleClient(conn, quit, end)
+			}()
+		} else {
+			//There was an error, so quit the server
+			end <- true;
+			break;
+		}
 	}
 	wg.Wait()
 }
 
-func handleClient(conn *net.UDPConn) {
+// Closes quit when correct input is detected. Closes quit if notified to do so through end channel
+// (for reporting of errors by other routines).
+func broadcastQuit(quit *chan bool, end *chan bool) {
+	for {
+		select {
+		case endserver := <-end:
+			if endserver {
+				close(quit)
+				break
+			}
+		}
+	}
+}
+
+// Waits for input on stdin. Sends end message if 'q' or EOF detected.
+func readIn(end *chan bool) {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, err := reader.ReadString('\n')
+
+		if text == "q" {
+			//post to channel or global
+			end <- true;
+			break;
+		}
+
+		if err == io.EOF {
+			end <- true;
+			break;
+		}
+	}
+}
+
+func handleClient(conn *net.UDPConn, quit *chan bool, end *chan bool) {
 	fmt.Println("Session created")
 
 	conn.SetReadDeadline(time.Now().Add(20 * time.Second))
@@ -39,12 +86,26 @@ func handleClient(conn *net.UDPConn) {
 
 		if err != nil {
 			if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
-				// we timed out, so send a goodbye message and close the connection
+				// we timed out
+				break;
 			}
-			// something else if messed up
+			//TODO: handle other errors
 		}
 
-		message := protocol.Decode(buf);
+		message := protocol.Decode(buf)
+
+		select {
+		case _,ok := <- quit:
+			if !ok {
+				break;
+			}
+		}
 		// logic for state transitions
+	}
+
+	//send goodbye message
+	err := conn.Close()
+	if err != nil {
+		end <- true
 	}
 }
